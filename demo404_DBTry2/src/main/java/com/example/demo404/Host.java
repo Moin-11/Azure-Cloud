18c1833e4e7b111ef7136ac29cb608d8e0684b66
moin <moin11.dev@gmail.com>
2023-11-26 01:57:22 +0400

    remoed unused imports

--- demo404_DBTry2/src/main/java/com/example/demo404/Host.java
+++ demo404_DBTry2/src/main/java/com/example/demo404/Host.java
@@ -4,10 +4,10 @@ import com.azure.spring.data.cosmos.core.mapping.Container;
 import com.azure.spring.data.cosmos.core.mapping.PartitionKey;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import org.springframework.data.annotation.Id;
+//import org.springframework.data.annotation.Id;
 
-import com.azure.spring.data.cosmos.core.mapping.Container;
-import com.azure.spring.data.cosmos.core.mapping.PartitionKey;
+//import com.azure.spring.data.cosmos.core.mapping.Container;
+//import com.azure.spring.data.cosmos.core.mapping.PartitionKey;
 
 @Container(containerName = "Hosts")
 public class Host {
--- demo404_DBTry2/src/main/java/com/example/demo404/HostController.java
+++ demo404_DBTry2/src/main/java/com/example/demo404/HostController.java
@@ -3,14 +3,14 @@ package com.example.demo404;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
+//import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
+//import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
